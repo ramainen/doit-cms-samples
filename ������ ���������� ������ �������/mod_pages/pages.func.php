9f44bb7cf765100fad1118a5140bebda11cd8bd2
ramainen <ainu.sky@gmail.com>
2011-10-25 23:01:12 +0400

    Реализация модуля страниц

---
+++ ������ ���������� ������ �������/mod_pages/pages.func.php
@@ -0,0 +1,20 @@
+<?php
+/*
+	Модуль для работы с текстовыми страницами, для вывода меню, выода подстраниц
+*/
+function pages_show()
+{
+	d()->page = d()->Page->find(url(1));
+	if (d()->page->is_empty) {
+		print "Страница не существует".d()->add(array('pages','url'=>url(1)));
+	} else {
+		print d()->pages_show_tpl();
+	}
+
+}
+
+ 
+function pages_tree()
+{
+	return d()->Page->tree(d()->Page->index);
+}
